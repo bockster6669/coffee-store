c754158a93935b76056fc8f072a30d9977598b0f
Versus Development <bockster6669@gmail.com>
2024-05-19 20:08:43 +0300

    update

---
+++ app/books/[name]/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function page({ params }: any) {
+  return <div>Book - {params.name}</div>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Banner from "@/components/Banner";
+
 export default function Home() {
   return (
     <main
       className="flex min-h-screen flex-col items-center
     justify-between p-14  "
     >
-      <h1>Coffee Connoisor</h1>
+      <Banner/>
     </main>
   );
 }
